1c1bd60c3065119bea0c50ed6d73905346cbc756
Syy <shirakawa.yy@gmail.com>
2018-09-30 21:45:01 +0900

    Add func to check bind lifecycle

--- Assets/Scripts/Test001/Test001Installer.cs
+++ Assets/Scripts/Test001/Test001Installer.cs
@@ -3,10 +3,28 @@ using Zenject;
 
 public class Test001Installer : MonoInstaller<Test001Installer>
 {
+    void Awake()
+    {
+        Debug.Log("Test001Installer Awake");
+    }
+
+    void Start()
+    {
+        Debug.Log("Test001Installer Start");
+    }
+
+    void Update()
+    {
+        Debug.Log("Test001Installer Update");
+    }
+
     public override void InstallBindings()
     {
+        Debug.Log("InstallBindings Start");
         Container.Bind<string>().FromInstance("string bind value");
         Container.Bind<Hoge>().AsSingle().NonLazy();
+
+        Debug.Log("InstallBindings End");
     }
 }
 
